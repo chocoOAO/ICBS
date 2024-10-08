978a567279571c05d1e9302f6c6e9e540263c377
chocoOAO <yves4416@gmail.com>
2024-10-08 18:55:02 +0800

    ts

--- predictmodel/test3.py
+++ predictmodel/test3.py
@@ -9,7 +9,6 @@ from tensorflow.keras.optimizers import Adam
 import re
 from sklearn.preprocessing import OneHotEncoder
 from tensorflow.keras.callbacks import EarlyStopping
-import random
 from tensorflow.keras.models import load_model
 
 # 標準參考數據
@@ -90,6 +89,40 @@ try:
                 for result in cursor:
                     feed_data_per_batch_number.append(result)
                 feed_data.append(feed_data_per_batch_number)
+                # 查詢每個 batchNumber 每天的重量和餵食數據，並將同一天的餵食數據進行聚合
+                query = f"""
+                    SELECT 
+                        rw.Date, 
+                        rw.weight, 
+                        GROUP_CONCAT(fl.feed_item SEPARATOR ',') AS feed_items,  -- 合併餵食類型
+                        SUM(fl.feed_quantity) AS total_feed_quantity  -- 聚合餵食量
+                    FROM 
+                        raw_weights rw
+                    LEFT JOIN 
+                        feeding_logs fl 
+                    ON 
+                        rw.batchNumber = fl.chicken_import_id 
+                    AND 
+                        rw.Date = fl.date 
+                    AND 
+                        fl.building_number = '{building_number}'
+                    WHERE 
+                        rw.batchNumber = '{batch}' 
+                    AND 
+                        rw.time = '18:00:00'  -- 只選取每天早上的記錄
+                    AND 
+                        rw.sid = '{sid}'
+                    GROUP BY 
+                        rw.Date, rw.weight  -- 添加 rw.weight 到 GROUP BY 中
+                    ORDER BY 
+                        rw.Date;
+                """
+
+                cursor.execute(query)
+                feed_data_per_batch_number = []
+                for result in cursor:
+                    feed_data_per_batch_number.append(result)
+                feed_data.append(feed_data_per_batch_number)
 
         # 查詢所有不同的飼料種類
         query = f"SELECT DISTINCT feed_item FROM feeding_logs;"
@@ -105,12 +138,134 @@ finally:
         cursor.close()
         connection.close()
         print("MySQL 連接已關閉")
+# 創建 DataFrame
+subset_feed_data = feed_data[:]
+initial_train_size = int(len(subset_feed_data) * 0.8)  # 初始訓練集大小
+validation_size = int(len(subset_feed_data) * 0.2)     # 驗證集大小
+correlation_results = {}  # 存储每个批次的相关系数
+for per_subset_feed_data in subset_feed_data[:initial_train_size]:
+    if not per_subset_feed_data:
+        continue
+    df = pd.DataFrame(per_subset_feed_data, columns=['Date','Weight','Feed_Type', 'Feed_Weight'])
+
+    df['Weight'] = df['Weight'].replace(0, np.nan)  # 將0值轉換為NaN
+    df['Feed_Weight'] = df['Feed_Weight'].fillna(0)  # 將NaN值轉換為0
+    df['Feed_Type'] = df['Feed_Type'].fillna('No_Feed')  # 將NaN值轉換為0
+    df['Weight'] = df['Weight'].combine_first(pd.Series(standard_data))  # 用standard_data對應位置的值填充NaN
+    df['Feed_Type'] = df.apply(lambda x: 'Mixed_Feed' if ',' in str(x['Feed_Type']) else x['Feed_Type'], axis=1)
+    df['Feed_Type'] = df.apply(
+    lambda x: 'Ｎ肉雞１號' if 'Ｎ肉雞１號' in str(x['Feed_Type']) else x['Feed_Type'],
+    axis=1
+    )
+    df['Feed_Type'] = df.apply(
+    lambda x: 'Ｎ肉雞2號' if 'Ｎ肉雞２號' in str(x['Feed_Type']) else x['Feed_Type'],
+    axis=1
+    )
+    df['Feed_Type'] = df.apply(
+    lambda x: 'Ｎ肉雞3號' if 'Ｎ肉雞３號' in str(x['Feed_Type']) else x['Feed_Type'],
+    axis=1
+    )
+    current_length = len(df['Weight'])
+    '''
+    # 假設 df 是你的 DataFrame
+    zero_feed_weight_count = (df['Feed_Weight'] != 0).sum()
+
+    if zero_feed_weight_count == 0:
+        # 取得 Feed_Weight 列的長度
+        length = len(df['Feed_Weight'])
+        
+        # 用來替換的值
+        replacement_value = 5566
+        
+        # 每三個替換一次
+        for i in range(0, length, 3):
+            df.loc[i, 'Feed_Weight'] = replacement_value  # 使用 .loc 替代 .iloc
+            df.loc[i, 'Feed_Type'] = 'Ｎ肉雞３號(添)  P'  # 使用 .loc 替代 .iloc
+    '''
+    # 如果长度小于 33
+    if current_length < 29:
+        
+    # 計算需要補充的數量
+    
+        needed_length = 33 - current_length
+        
+        # 從 standard_data_df 補充數據
+        df_additional = standard_data_df.iloc[current_length:current_length+needed_length].reset_index(drop=True)
+        last_date = df['Date'].iloc[-1]  # 獲取最後一個日期
+
+        # 更新 Weight 和 Date
+        df = pd.concat([df, df_additional], ignore_index=True)
+        
+        # 更新日期
+        new_dates = [last_date + pd.Timedelta(days=i) for i in range(1, needed_length + 1)]
+        
+        # 使用 .loc 來賦值，避免警告
+        df.loc[current_length:, 'Date'] = new_dates
+         # 補充 Feed_Weight 為 0
+        df.loc[current_length:, 'Feed_Weight'] = 0
+        
+        # 補充 Feed_Type 為 'No_Feed'
+        df.loc[current_length:, 'Feed_Type'] = 'No_Feed'
+        
+        # 输出结果
+    # 明確指定要進行編碼的類別
+    categories = [['No_Feed', 'Mixed_Feed','Ｎ肉雞１號','Ｎ肉雞2號','Ｎ肉雞3號']]  # 所有可能的類別
+    # 初始化編碼器，並設置 `handle_unknown='ignore'`
+    encoder = OneHotEncoder(categories=categories, handle_unknown='ignore', sparse=False)
+
+    # 進行編碼
+    encoded_features = encoder.fit_transform(df[['Feed_Type']])
+
+    # 將編碼後的特徵轉換為 DataFrame
+    encoded_df = pd.DataFrame(encoded_features, columns=encoder.get_feature_names_out(['Feed_Type']))
+
+    # 合併編碼後的特徵到原始數據集
+    df_encoded = pd.concat([df.drop(columns=['Feed_Type']), encoded_df], axis=1)
+    print(df_encoded.columns)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    data_scaled = scaler.fit_transform(df_encoded[['Weight', 'Feed_Weight'] + [col for col in df_encoded.columns if 'Feed_Type' in col]])
+
+    # LSTM 模型需要 3D 輸入數據
+    X_train = []
+    y_train = []
+
+    # 使用過去5天數據預測未來1天
+    for i in range(5, len(data_scaled)):
+        X_train.append(data_scaled[i-5:i, :])  # 將過去5天的所有特徵添加到 X_train
+        y_train.append(data_scaled[i, 0])      # 目標是當前的 Weight
+
+    X_train, y_train = np.array(X_train), np.array(y_train)
+
+    # 調整輸入數據格式為 LSTM 所需的 3D 格式
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], X_train.shape[2]))
+    n_step = X_train.shape[1]  # 這是時間步長
+    n_feature = X_train.shape[2]  # 這是特徵數
+    # 构建并优化 LSTM 模型
+    model = Sequential()
+
+    # 調整 LSTM 層單元數及 Dropout
+    model.add(LSTM(units=50, activation='relu',return_sequences=False, input_shape=(n_step, n_feature)))
+    model.add(Dense(units=1))
+
+    model.compile(optimizer='adam', loss='mse', metrics=['mse', 'mape'])
+
+    # 加载之前保存的模型权重
+    try:
+        model.load_weights("new_model.h5")
+        print("成功加载模型权重。")
+    except:
+        print("未找到保存的权重文件，开始从头训练模型。")
+
+    # 進行模型訓練
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+
+    model.fit(X_train, y_train, epochs=100, batch_size=20, validation_split=0.2, callbacks=[early_stopping])
+
+    # 保存模型
+    model.save("new_model.h5")
 count = 0
-# 隨機化數據
-random.shuffle(feed_data)
-subset_data = [entry for entry in feed_data[:20]]  # 取出每個 batch 的前 21 筆數據
-for data_per_batch_number in subset_data:
-    origin_df = pd.DataFrame(feed_data[count], columns=['Date', 'Weight', 'Feed_Type', 'Feed_Weight'])
+for data_per_batch_number in subset_feed_data[initial_train_size:]:
+    origin_df = pd.DataFrame(feed_data[count+initial_train_size], columns=['Date', 'Weight', 'Feed_Type', 'Feed_Weight'])
     df = pd.DataFrame(data_per_batch_number, columns=['Date', 'Weight', 'Feed_Type', 'Feed_Weight'])
 
     df['Weight'] = df['Weight'].replace(0, np.nan)  # 將0值轉換為NaN
